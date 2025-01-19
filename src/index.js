e9af94d9cba9cc62f849e966dc9c38dcb88ba5d0
pkb32 <113055313+pkb32@users.noreply.github.com>
2025-01-19 12:10:40 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,35 @@
+//require ('dotenv').config({path: './env'});
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+
+
+/* to directly connect the DB in the index.js file
+import express from "express";
+const app = express();
+
+//IIFE
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        app.on("error",(error)=>{
+            console.log("Err:",error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+})();*/
