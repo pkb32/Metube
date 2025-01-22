2002cc2be710985424e86c3ee98c197ef8afd3b6
pkb32 <113055313+pkb32@users.noreply.github.com>
2025-01-22 18:56:27 +0530

    register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -51,7 +51,7 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     //3
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or:[{username},{email}]
     })
 
@@ -62,7 +62,13 @@ const registerUser = asyncHandler(async (req, res) => {
     //4
     //eeita multer dela .files ra field re
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+        
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar is required");
